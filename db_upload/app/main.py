0ccdec5bc74ce89c122287349b62087c856afa31
Kvl61112 <email@pollakrobert.com>
2025-11-10 16:01:10 +0100

    0.0.1

--- db_upload/app/main.py
+++ db_upload/app/main.py
@@ -7,8 +7,25 @@ def main():
     print("App Started.")
 
     # Táblázat beolvasás és manipuláció
-    df_raw = Data.get_df_from_csv("data/raw_panel.csv")
-    print(df_raw)
+    df = Data.get_df_from_csv("data/raw_panel.csv")
+
+
+    # 2 oszloponként lépünk végig (0, 2, 4, ..., 26)
+    for i in range(0, df.shape[1], 2):
+        # kiválasztjuk a 2 oszlopot
+        sub = df.iloc[:, i:i+2].copy()
+
+        # új DataFrame, ahol:
+        # - "index_id" az aktuális panel (pl. i//2)
+        # - a 2. és 3. oszlop a kivágott adatok
+        new_df = pd.DataFrame({
+            "panel_index": [i//2] * len(sub),  # minden sorhoz ugyanaz az index
+            "col_1": sub.iloc[:, 0].values,
+            "col_2": sub.iloc[:, 1].values
+        })
+
+
+    print(df)
 
     #db = Database("mysqldb",3306,"dm","dmpass")
     db = Database("127.0.0.1",3306,"db_bead","db_pass")
